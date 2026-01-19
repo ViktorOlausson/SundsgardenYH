7728eb9a3c793e5990e38e0d011d82c90b8d61e4
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2026-01-19 11:11:34 +0100

    some easy first exercises

---
+++ Week 1/index.js
@@ -0,0 +1,11 @@
+const firstName = "Viktor"
+const age = 23
+const isMale = true
+// console.log(firstName, age, isMale)
+console.log(`My name is ${firstName}, I'm ${age} year old and am i a male? ${isMale}`)
+
+const favFood = "Rice with salmon"
+const eatPerWeek = "2-3 times"
+const canCook = true
+
+console.log(`my favorite food is ${favFood}, i eat it about ${eatPerWeek}, ${canCook ? "i can cook it my self" : "i can not cook it my self"}`)
