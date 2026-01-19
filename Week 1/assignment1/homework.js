95756040de437d9aac265f1f4341181550bba2a6
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2026-01-19 16:40:54 +0100

    part 1 done

---
+++ Week 1/assignment1/homework.js
@@ -0,0 +1,5 @@
+var firstName = "Viktor"
+var favNum = 17
+var likeCoding = true
+
+// console.log(`Hi, my name is ${firstName}, my favorite number is ${favNum}, and it is ${likeCoding} that I like coding.`)
