0a59f6320f8c8269037f09a615f08acf06a1532d
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2026-01-20 13:20:40 +0100

    level 1 done

---
+++ course1/Week1/assignment2/beginner.js
@@ -0,0 +1,19 @@
+function sayHello(){
+    console.log("Hello from node.js");
+}
+
+//sayHello()
+
+function welcomeUser(name){
+    console.log(`Welcome ${name}!`);
+}
+
+// welcomeUser("Username")
+
+function subtract(a,b){
+    const result = a - b
+
+    console.log(result);
+}
+
+subtract(10,3)
