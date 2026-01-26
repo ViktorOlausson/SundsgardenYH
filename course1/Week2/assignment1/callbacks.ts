232476bd3f200349d6b3ebc34a00c7b70306a3a5
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2026-01-26 20:26:36 +0100

    nr 2 done

--- course1/Week2/assignment1/callbacks.ts
+++ course1/Week2/assignment1/callbacks.ts
@@ -1,11 +1,21 @@
 type printType = (data:string) => void;
 
+//nr 1
 function printHello(print: printType){
     print("Hello from callback!")
+}
+
+//nr2
+
+function printLateHello(print: printType){
+    setTimeout(() => {
+        print("Hi, I am late!")
+    }, 2000);
 }
 
 function print(message: string){
     console.log(message);
 }
 
-printHello(print)
+//printHello(print)
+printLateHello(print)
