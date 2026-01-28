e2f535cfb24937cebd5f757c7276826664fd00fb
Viktor Olausson <02viktorolausson@gmail.com>
2026-01-28 15:41:08 +0100

    exercise 1, OR operator

--- course1/Week2/assignment2/random-exercises.ts
+++ course1/Week2/assignment2/random-exercises.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+type IDType = string | number
+
+function printID(_ID:IDType){
+    console.log(`Your ID is: ${_ID}`);
+}
+
+let id1: IDType = "thisIsAStringID"
+let id2: IDType = 123
+
+printID(id1)
+printID(id2)
