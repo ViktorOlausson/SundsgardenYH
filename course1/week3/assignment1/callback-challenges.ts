76eb10597b2eee85fb03fed0d7a4e5c31dc42f56
Viktor Olausson <02viktorolausson@gmail.com>
2026-02-02 11:09:52 +0100

    challenge 2, addS function

--- course1/week3/assignment1/callback-challenges.ts
+++ course1/week3/assignment1/callback-challenges.ts
@@ -7,5 +7,18 @@ function addTwo(input: number){
 }
 
 // To check if you've completed it, uncomment these console.logs!
-console.log(addTwo(3));
-console.log(addTwo(10));
+//console.log(addTwo(3));
+//console.log(addTwo(10));
+
+// ________________________________________________________________________________________________
+// Challenge 2
+/* 
+Create a function addS that accepts one input and adds an "s" to it.
+*/
+
+function addS(input: string){
+    return input + "s"
+}
+// uncomment these to check your work
+console.log(addS("pizza"));
+console.log(addS("bagel"));
