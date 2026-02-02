08f9c103f8b2cb464b1031824e8fe1bad60c0435
Viktor Olausson <02viktorolausson@gmail.com>
2026-02-02 11:08:04 +0100

    challenge 1, addTwo function

---
+++ course1/week3/assignment1/callback-challenges.ts
@@ -0,0 +1,11 @@
+//i write the old way becuase thats what i like, it makes more sense to me becuase of my C# background
+// Challenge 1
+/*  Create a function addTwo that accepts one input and adds 2 to it. */
+
+function addTwo(input: number){
+    return input + 2
+}
+
+// To check if you've completed it, uncomment these console.logs!
+console.log(addTwo(3));
+console.log(addTwo(10));
