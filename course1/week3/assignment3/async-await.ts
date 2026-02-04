f376adcfd7658da5626094f804859339ae024fd8
Viktor Olausson <02viktorolausson@gmail.com>
2026-02-04 16:17:53 +0100

    part 1 done

---
+++ course1/week3/assignment3/async-await.ts
@@ -0,0 +1,41 @@
+//part 1:
+function coinFlip () { 
+    return new Promise((resolve, reject) => {
+        let result: number = Math.random()
+        if(result > 0.5){resolve("you win")}
+        else{reject("you lose")}
+    })
+}
+
+async function coinFlipResult(){
+    try{
+        const result = await coinFlip()
+        console.log(result);
+    }catch (error){
+        console.log(error);
+    }
+}
+
+coinFlipResult()
+
+type jokeResponse = {
+    id: string,
+    value: string
+}
+
+function fetchJoke(){
+    const promise: Promise<void> = fetch("https://api.chucknorris.io/jokes/random").then((response:Response) => {
+        if(!response.ok){
+            throw new Error("faiiiiiil")
+        }
+        return response.json() as Promise<jokeResponse>
+    }
+    ).then((data) => {
+        const joke: string = data.value
+        console.log(joke);
+    }).catch((error) => {
+        console.log(error);
+    })
+}
+
+// fetchJoke()
