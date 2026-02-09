15a76607d1fa796ae9ffd3198ec5057be6a81f63
Viktor Olausson <02viktorolausson@gmail.com>
2026-02-09 21:44:56 +0100

    task 2 done

--- course1/week4/assignment1/eslintTasks.ts
+++ course1/week4/assignment1/eslintTasks.ts
@@ -1,11 +1,17 @@
 const greet = (firstName: string) => {
-  return `Hello ${firstName}`
-}
+  return `Hello ${firstName}`;
+};
 
-greet("Student")
+greet("Student");
 
 const double = (n: number) => {
-  return n * 2
-}
+  return n * 2;
+};
 
-console.log(`result${double(5)}`)
+console.log(`result${double(5)}`);
+
+const isEven = (num: number) => {
+  return num % 2 === 0;
+};
+
+console.log(`Test: ${isEven(2)}`);
