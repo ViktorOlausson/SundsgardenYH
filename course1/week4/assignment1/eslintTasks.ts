834ae9e7de09d3d72ccccbbc89f79f4457fa49b7
Viktor Olausson <02viktorolausson@gmail.com>
2026-02-09 21:40:33 +0100

    eslint init, prettier init, task 1 & 2 done

---
+++ course1/week4/assignment1/eslintTasks.ts
@@ -0,0 +1,11 @@
+const greet = (firstName: string) => {
+  return `Hello ${firstName}`
+}
+
+greet("Student")
+
+const double = (n: number) => {
+  return n * 2
+}
+
+console.log(`result${double(5)}`)
