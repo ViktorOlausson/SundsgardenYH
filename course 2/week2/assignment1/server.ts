ecabfe665b8a6af4726048d3b31f20949602cfe5
Viktor Olausson <02viktorolausson@gmail.com>
2026-03-02 14:59:45 +0100

    HTTP get kicks

---
+++ course 2/week2/assignment1/server.ts
@@ -0,0 +1,35 @@
+import express from "express";
+//sample data
+// kName = Korean Name
+// eName = english Name
+const TkdKicks = [
+  { id: 1, kName: "Ap Chagi", eName: "Front Kick", difficulty: "Easy" },
+  { id: 2, kName: "Dolloy Chagi", eName: "Round Kick", difficulty: "Medium" },
+  {
+    id: 3,
+    kName: "Bandae Dolloy Chagi",
+    eName: "Reverse Round Kick",
+    difficulty: "Hard",
+  },
+];
+
+const app = express();
+
+const PORT = 3000;
+
+app.get(`/TkdKicks`, (req, res) => {
+  res.json(TkdKicks);
+});
+
+app.get(`/TkdKicks/:id`, (req, res) => {
+  const kickId = parseInt(req.params.id);
+  const kick = TkdKicks.find((k) => k.id === kickId);
+  if (!kick) {
+    return res.status(404).json({ message: "Kick not found" });
+  }
+  res.json({ message: "Kick found", kick });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
