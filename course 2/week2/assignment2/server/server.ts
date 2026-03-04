0e268b8b9a218d3cabc98dcb80408620d62646b7
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2026-03-04 22:26:27 +0100

    Basic server

---
+++ course 2/week2/assignment2/server/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
