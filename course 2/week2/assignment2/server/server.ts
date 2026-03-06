a0e3b66b0ca99ed6b3bcb5ee06e02b4e486b168b
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2026-03-06 15:17:00 +0100

    zod validated fetch random person HTTP req

--- course 2/week2/assignment2/server/server.ts
+++ course 2/week2/assignment2/server/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { z } from "zod";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+type personResponse = {};
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
@@ -11,4 +14,41 @@ app.listen(PORT, () => {
 
 app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
+});
+
+const randomPersonResponseSchema = z.object({
+  results: z.array(
+    z.object({
+      name: z.object({
+        first: z.string(),
+        last: z.string(),
+      }),
+      location: z.object({
+        country: z.string(),
+      }),
+    }),
+  ),
+});
+
+app.get("/random-person", async (req, res) => {
+  try {
+    const response = await fetch("https://randomuser.me/api/");
+    const data = await response.json();
+    const validRandomPerson = randomPersonResponseSchema.safeParse(data);
+    if (!validRandomPerson.success) {
+      return res.status(500).json({
+        message: "Failed to validate user",
+        details: validRandomPerson.error,
+      });
+    }
+    const user = validRandomPerson.data.results[0];
+    res.json({
+      name: `${user?.name.first} ${user?.name.last}`,
+      country: user?.location.country,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Failed to fetch user",
+    });
+  }
 });
