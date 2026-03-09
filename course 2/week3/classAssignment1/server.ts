4e6a3becddac03a77440ab9edf7ccf001623a58a
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2026-03-09 15:16:45 +0100

    post safe pare user

--- course 2/week3/classAssignment1/server.ts
+++ course 2/week3/classAssignment1/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { email, z } from "zod";
+import { email, z, type TypeOf } from "zod";
 
 const app = express();
 const PORT = 3000;
@@ -12,8 +12,10 @@ const userSchema = z.object({
   email: z.email(),
   age: z.number().int().positive().optional(),
   role: z.enum(["admin", "user", "guest"]),
-  createdAt: z.date(),
+  createdAt: z.coerce.date(),
 });
+
+type user = z.infer<typeof userSchema>;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
@@ -21,4 +23,30 @@ app.listen(PORT, () => {
 
 app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
+});
+
+app.post("/safeUser", (req, res) => {
+  try {
+    const validUser = userSchema.safeParse(req.body);
+    if (!validUser.success) {
+      return res.status(400).json({
+        message: "Failed to validate user",
+        details: validUser.error,
+      });
+    }
+    const user = validUser.data;
+    res.status(201).json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      age: user.age,
+      role: user.role,
+      createdAt: user.createdAt,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Failed to post user",
+      details: err,
+    });
+  }
 });
