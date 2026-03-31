b1b2bb18fb403e0a08d1ee9e00b6d954ea93106a
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2026-03-31 11:24:00 +0200

    post new product

--- course 2/week6/assignment2/server.ts
+++ course 2/week6/assignment2/server.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
 
 const app = express();
 app.use(express.json());
 const PORT = 3000;
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:3000`);
@@ -10,4 +21,19 @@ app.listen(PORT, () => {
 
 app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
+});
+
+app.post("/products", async (req, res) => {
+  try {
+    const newProduct = await prisma.product.create({ data: req.body });
+    res.json(newProduct);
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(500).json({
+        message: err.message,
+        reason: "parse to json",
+      });
+    }
+    res.status(500).send("Unknown error");
+  }
 });
